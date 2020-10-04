e0d5e947eb0d6c7eea2d66b4664c636dffbda584
Deji <wrecodde@gmail.com>
2020-10-04 20:53:39 +0100

    removed drf's html renderer

--- backend/settings.py
+++ backend/settings.py
@@ -144,3 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
